ce7061b4f2e7d1a32397860695eca440d42d0e82
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2021-11-28 20:11:08 -0300

    setup inicial

---
+++ ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -0,0 +1,22 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+import java.util.Map;
+import java.util.Properties;
+
+public class NewOrderMain {
+
+    public static void main(String[] args) {
+
+        var producer = new KafkaProducer<String, String>(properties());
+
+
+
+    }
+
+    private static Properties properties() {
+        return null;
+    }
+
+}
